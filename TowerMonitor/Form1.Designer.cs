68022935f3611b9f6e8b269ce0745cc367e2bed0
Sonny Shih <sonnyshih@gmail.com>
2022-12-15 14:28:26 +0800

    加入專案檔案。

---
+++ TowerMonitor/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace TowerMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.realtimePictureBox = new System.Windows.Forms.PictureBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.accountLabel = new System.Windows.Forms.Label();
+            this.portTextBox = new System.Windows.Forms.TextBox();
+            this.portLabel = new System.Windows.Forms.Label();
+            this.ipTextBox = new System.Windows.Forms.TextBox();
+            this.ipLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.channelTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.realtimePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.realtimePictureBox);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.channelTextBox);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.loginButton);
+            this.splitContainer1.Panel2.Controls.Add(this.passwordTextBox);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.usernameTextBox);
+            this.splitContainer1.Panel2.Controls.Add(this.accountLabel);
+            this.splitContainer1.Panel2.Controls.Add(this.portTextBox);
+            this.splitContainer1.Panel2.Controls.Add(this.portLabel);
+            this.splitContainer1.Panel2.Controls.Add(this.ipTextBox);
+            this.splitContainer1.Panel2.Controls.Add(this.ipLabel);
+            this.splitContainer1.Size = new System.Drawing.Size(934, 555);
+            this.splitContainer1.SplitterDistance = 688;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // realtimePictureBox
+            // 
+            this.realtimePictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.realtimePictureBox.Location = new System.Drawing.Point(0, 0);
+            this.realtimePictureBox.Name = "realtimePictureBox";
+            this.realtimePictureBox.Size = new System.Drawing.Size(688, 555);
+            this.realtimePictureBox.TabIndex = 0;
+            this.realtimePictureBox.TabStop = false;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.loginButton.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.loginButton.Location = new System.Drawing.Point(30, 187);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(189, 28);
+            this.loginButton.TabIndex = 8;
+            this.loginButton.Text = "登入";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.onLoginClick);
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.passwordTextBox.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.passwordTextBox.Location = new System.Drawing.Point(84, 120);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(135, 27);
+            this.passwordTextBox.TabIndex = 7;
+            this.passwordTextBox.Text = "Seafoodlab7812";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(26, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 21);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "密碼:";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.usernameTextBox.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.usernameTextBox.Location = new System.Drawing.Point(84, 87);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(135, 27);
+            this.usernameTextBox.TabIndex = 5;
+            this.usernameTextBox.Text = "admin";
+            // 
+            // accountLabel
+            // 
+            this.accountLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.accountLabel.AutoSize = true;
+            this.accountLabel.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.accountLabel.Location = new System.Drawing.Point(26, 88);
+            this.accountLabel.Name = "accountLabel";
+            this.accountLabel.Size = new System.Drawing.Size(57, 21);
+            this.accountLabel.TabIndex = 4;
+            this.accountLabel.Text = "帳號:";
+            // 
+            // portTextBox
+            // 
+            this.portTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.portTextBox.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.portTextBox.Location = new System.Drawing.Point(84, 54);
+            this.portTextBox.Name = "portTextBox";
+            this.portTextBox.Size = new System.Drawing.Size(135, 27);
+            this.portTextBox.TabIndex = 3;
+            this.portTextBox.Text = "8000";
+            // 
+            // portLabel
+            // 
+            this.portLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.portLabel.AutoSize = true;
+            this.portLabel.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.portLabel.Location = new System.Drawing.Point(35, 55);
+            this.portLabel.Name = "portLabel";
+            this.portLabel.Size = new System.Drawing.Size(48, 21);
+            this.portLabel.TabIndex = 2;
+            this.portLabel.Text = "Port:";
+            // 
+            // ipTextBox
+            // 
+            this.ipTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ipTextBox.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.ipTextBox.Location = new System.Drawing.Point(84, 21);
+            this.ipTextBox.Name = "ipTextBox";
+            this.ipTextBox.Size = new System.Drawing.Size(135, 27);
+            this.ipTextBox.TabIndex = 1;
+            this.ipTextBox.Text = "192.168.50.137";
+            // 
+            // ipLabel
+            // 
+            this.ipLabel.AutoSize = true;
+            this.ipLabel.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.ipLabel.Location = new System.Drawing.Point(50, 21);
+            this.ipLabel.Name = "ipLabel";
+            this.ipLabel.Size = new System.Drawing.Size(33, 21);
+            this.ipLabel.TabIndex = 0;
+            this.ipLabel.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(26, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 21);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "頻道:";
+            // 
+            // channelTextBox
+            // 
+            this.channelTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.channelTextBox.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.channelTextBox.Location = new System.Drawing.Point(84, 154);
+            this.channelTextBox.Name = "channelTextBox";
+            this.channelTextBox.Size = new System.Drawing.Size(135, 27);
+            this.channelTextBox.TabIndex = 11;
+            this.channelTextBox.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 555);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.realtimePictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox realtimePictureBox;
+        private System.Windows.Forms.TextBox ipTextBox;
+        private System.Windows.Forms.Label ipLabel;
+        private System.Windows.Forms.Label portLabel;
+        private System.Windows.Forms.TextBox portTextBox;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.Label accountLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox channelTextBox;
+    }
+}
+
---
+++ TowerMonitor/Form1.cs
@@ -0,0 +1,244 @@
+﻿using HikvisionDemo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static HikvisionDemo.CHCNetSDK;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TowerMonitor
+{
+    public partial class Form1 : Form
+    {
+        public uint PTZ_MOVING = 0;         // 雲台移動
+        public uint PTZ_STOP = 1;           // 雲台停止
+        public uint PTZ_SPEED = 3;          // 雲台移動速度 (預設 3)
+        private Int32 m_lRealHandle = -1;   // 預覽接口
+        public int m_lChannel = 1;          // 通道號碼 (預設 1)
+        private uint iLastErr = 0;          // Error Code
+        private bool m_bInitSDK = false;
+        private Int32 m_lUserID = -1;       // User ID
+        private string str;
+        private bool isAuto = false;        // 雲台自動移動
+
+        CHCNetSDK.LOGINRESULTCALLBACK loginCallBack = null;
+        CHCNetSDK.NET_DVR_USER_LOGIN_INFO struLogInfo;
+        CHCNetSDK.NET_DVR_DEVICEINFO_V40 DeviceInfo;
+        CHCNetSDK.REALDATACALLBACK RealData = null;
+        public delegate void UpdateTextStatusCallback(string strLogStatus, IntPtr lpDeviceInfo);
+
+        public Form1()
+        {
+            InitializeComponent();
+            m_bInitSDK = CHCNetSDK.NET_DVR_Init();
+            if (m_bInitSDK == false)
+            {
+                MessageBox.Show("NET_DVR_Init error!");
+                return;
+            }
+            else
+            {
+                // To save the SDK log
+                //CHCNetSDK.NET_DVR_SetLogToFile(3, "C:\\SdkLog\\", true);
+            }
+        }
+
+        private void onLoginClick(object sender, EventArgs e)
+        {
+            if (ipTextBox.Text == "" || portTextBox.Text == "" ||
+                usernameTextBox.Text == "" || passwordTextBox.Text == "")
+            {
+                MessageBox.Show("請輸入IP、Port、帳號、密碼");
+                return;
+            }
+
+            if (m_lUserID < 0)
+            {
+
+                struLogInfo = new CHCNetSDK.NET_DVR_USER_LOGIN_INFO();
+
+                //Device IP or web url
+                byte[] byIP = System.Text.Encoding.Default.GetBytes(ipTextBox.Text);
+                struLogInfo.sDeviceAddress = new byte[129];
+                byIP.CopyTo(struLogInfo.sDeviceAddress, 0);
+
+                // Device Service Port
+                struLogInfo.wPort = ushort.Parse(portTextBox.Text);
+
+                //Username
+                byte[] byUserName = System.Text.Encoding.Default.GetBytes(usernameTextBox.Text);
+                struLogInfo.sUserName = new byte[64];
+                byUserName.CopyTo(struLogInfo.sUserName, 0);
+
+                //Password
+                byte[] byPassword = System.Text.Encoding.Default.GetBytes(passwordTextBox.Text);
+                struLogInfo.sPassword = new byte[64];
+                byPassword.CopyTo(struLogInfo.sPassword, 0);
+
+
+                if (loginCallBack == null)
+                {
+                    loginCallBack = new CHCNetSDK.LOGINRESULTCALLBACK(cbLoginCallBack);     //注册回调函数                    
+                }
+                struLogInfo.cbLoginResult = loginCallBack;
+                struLogInfo.bUseAsynLogin = false; //是否异步登录：0- 否，1- 是 
+
+                DeviceInfo = new CHCNetSDK.NET_DVR_DEVICEINFO_V40();
+
+                // Login the device
+                m_lUserID = CHCNetSDK.NET_DVR_Login_V40(ref struLogInfo, ref DeviceInfo);
+                if (m_lUserID < 0)
+                {
+                    iLastErr = CHCNetSDK.NET_DVR_GetLastError();
+                    str = "NET_DVR_Login_V40 failed, error code= " + iLastErr; //登录失败，输出错误号
+                    MessageBox.Show(str);
+                    return;
+                }
+                else
+                {
+                    //登录成功
+                    MessageBox.Show("登入成功!");
+                    loginButton.Text = "登出";
+                    StartPreview();
+                }
+
+            }
+            else
+            {
+                //注销登录 Logout the device
+                //if (m_lRealHandle >= 0)
+                //{
+                //    MessageBox.Show("請先停止畫面預覽");
+                //    return;
+                //}
+
+                StopPreview();
+
+                if (!CHCNetSDK.NET_DVR_Logout(m_lUserID))
+                {
+                    iLastErr = CHCNetSDK.NET_DVR_GetLastError();
+                    str = "NET_DVR_Logout failed, error code= " + iLastErr;
+                    MessageBox.Show(str);
+                    return;
+                }
+                m_lUserID = -1;
+                MessageBox.Show("登出成功!");
+                loginButton.Text = "登入";
+            }
+            return;
+        }
+
+        public void cbLoginCallBack(int lUserID, int dwResult, IntPtr lpDeviceInfo, IntPtr pUser)
+        {
+            string strLoginCallBack = "登入設備，lUserID：" + lUserID + "，dwResult：" + dwResult;
+
+            if (dwResult == 0)
+            {
+                uint iErrCode = CHCNetSDK.NET_DVR_GetLastError();
+                strLoginCallBack = strLoginCallBack + "，錯誤號:" + iErrCode;
+            }
+
+            //下面代码注释掉也会崩溃
+            if (InvokeRequired)
+            {
+                object[] paras = new object[2];
+                paras[0] = strLoginCallBack;
+                paras[1] = lpDeviceInfo;
+                statusLabel.BeginInvoke(new UpdateTextStatusCallback(UpdateClientList), paras);
+            }
+            else
+            {
+                //创建该控件的主线程直接更新信息列表 
+                UpdateClientList(strLoginCallBack, lpDeviceInfo);
+            }
+
+        }
+
+        public void UpdateClientList(string strLogStatus, IntPtr lpDeviceInfo)
+        {
+            //列表新增报警信息
+            //statusLabel.Text = "登入狀態: " + strLogStatus;
+        }
+
+        private void StartPreview() {
+            if (m_lRealHandle < 0)
+            {
+                CHCNetSDK.NET_DVR_PREVIEWINFO lpPreviewInfo = new CHCNetSDK.NET_DVR_PREVIEWINFO();
+                lpPreviewInfo.hPlayWnd = realtimePictureBox.Handle;         // 预览窗口
+                lpPreviewInfo.lChannel = Int16.Parse(channelTextBox.Text);  // 预览的设备通道
+                lpPreviewInfo.dwStreamType = 0;                             // 码流类型：0-主码流，1-子码流，2-码流3，3-码流4，以此类推
+                lpPreviewInfo.dwLinkMode = 0;                               // 连接方式：0- TCP方式，1- UDP方式，2- 多播方式，3- RTP方式，4-RTP/RTSP，5-RSTP/HTTP 
+                lpPreviewInfo.bBlocked = true;                              // 0- 非阻塞取流，1- 阻塞取流
+                lpPreviewInfo.dwDisplayBufNum = 1;                          // 播放库播放缓冲区最大缓冲帧数
+                lpPreviewInfo.byProtoType = 0;
+                lpPreviewInfo.byPreviewMode = 0;
+
+                //if (idTextBox.Text != "")
+                //{
+                //    lpPreviewInfo.lChannel = -1;
+                //    byte[] byStreamID = System.Text.Encoding.Default.GetBytes(idTextBox.Text);
+                //    lpPreviewInfo.byStreamID = new byte[32];
+                //    byStreamID.CopyTo(lpPreviewInfo.byStreamID, 0);
+                //}
+
+
+                if (RealData == null)
+                {
+                    RealData = new CHCNetSDK.REALDATACALLBACK(RealDataCallBack);    //预览实时流回调函数
+                }
+
+                IntPtr pUser = new IntPtr();//用户数据
+
+                //Start live view 
+                m_lRealHandle = CHCNetSDK.NET_DVR_RealPlay_V40(m_lUserID, ref lpPreviewInfo, null/*RealData*/, pUser);
+
+                if (m_lRealHandle < 0)
+                {
+                    iLastErr = CHCNetSDK.NET_DVR_GetLastError();
+                    str = "NET_DVR_RealPlay_V40 failed, error code= " + iLastErr; //预览失败，输出错误号
+                    MessageBox.Show(str);
+                    return;
+                }
+            }
+        }
+
+        public void RealDataCallBack(Int32 lRealHandle, UInt32 dwDataType, IntPtr pBuffer, UInt32 dwBufSize, IntPtr pUser)
+        {
+            if (dwBufSize > 0)
+            {
+                byte[] sData = new byte[dwBufSize];
+                Marshal.Copy(pBuffer, sData, 0, (Int32)dwBufSize);
+
+                string str = "实时流数据.ps";
+                FileStream fs = new FileStream(str, FileMode.Create);
+                int iLen = (int)dwBufSize;
+                fs.Write(sData, 0, iLen);
+                fs.Close();
+            }
+        }
+
+        private void StopPreview() {
+
+            if (m_lRealHandle >= 0) {
+                // Stop live view 
+                if (!CHCNetSDK.NET_DVR_StopRealPlay(m_lRealHandle))
+                {
+                    iLastErr = CHCNetSDK.NET_DVR_GetLastError();
+                    str = "NET_DVR_StopRealPlay failed, error code= " + iLastErr;
+                    MessageBox.Show(str);
+                    return;
+                }
+                m_lRealHandle = -1;
+                //priviewButton.Text = "顯示及時影像";
+            }
+
+        }
+    }
+}
